a012b789d0b777f1a793485a086144749cec83d5
dev-from-cbd <veryfatwombat@gmail.com>
2021-12-19 08:57:42 +1100

    react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React from "react";
 import "./App.css";
+
 import { render } from "react-dom";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state={
-      items:[],
-      currebtItem:{
-        
-      }
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      },
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  }
+
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    //console.log(newItem);
+
+    if (newItem.text !== "") {
+      const newItems = [...this.state.items, newItems];
+      this.setState({
+        items: newItems,
+        currentItems: {
+          text: "",
+          key: "",
+        },
+      });
     }
   }
 
@@ -18,8 +49,13 @@ class App extends React.Component {
     return (
       <div className="App">
         <header>
-          <form id="to-do-form">
-            <input type="text" placeholder="Enter any task" />
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Enter any task"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
             <button type="submit">Add</button>
           </form>
         </header>
