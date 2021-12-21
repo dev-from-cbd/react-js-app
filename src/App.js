53596d0c2e2e199927c15d9e5c77d0d397b9456e
dev-from-cbd <veryfatwombat@gmail.com>
2021-12-22 00:11:01 +1100

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import ListItems from "./ListItems";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-//import { render } from "react-dom";
+library.add(faTrash);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       items: [],
       currentItem: {
@@ -15,11 +17,25 @@ class App extends React.Component {
         key: "",
       },
     };
-
-    this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
   }
-
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.text !== "") {
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: {
+          text: "",
+          key: "",
+        },
+      });
+    }
+  }
   handleInput(e) {
     this.setState({
       currentItem: {
@@ -28,24 +44,25 @@ class App extends React.Component {
       },
     });
   }
-
-  addItem(e) {
-    e.preventDefault();
-    const newItem = this.state.currentItem;
-    //console.log(newItem);
-
-    if (newItem.text !== "") {
-      const newItems = [...this.state.items, newItems];
-      this.setState({
-        items: newItems,
-        currentItems: {
-          text: "",
-          key: "",
-        },
-      });
-    }
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter((item) => item.key !== key);
+    this.setState({
+      items: filteredItems,
+    });
   }
-
+  setUpdate(text, key) {
+    console.log("items:" + this.state.items);
+    const items = this.state.items;
+    items.map((item) => {
+      if (item.key === key) {
+        console.log(item.key + "    " + key);
+        item.text = text;
+      }
+    });
+    this.setState({
+      items: items,
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -53,14 +70,19 @@ class App extends React.Component {
           <form id="to-do-form" onSubmit={this.addItem}>
             <input
               type="text"
-              placeholder="Enter any your task"
+              placeholder="Enter your task"
               value={this.state.currentItem.text}
               onChange={this.handleInput}
-            />
+            ></input>
             <button type="submit">Add</button>
           </form>
+          <p>{this.state.items.text}</p>
 
-          <ListItems items={this.state.items} />
+          <ListItems
+            items={this.state.items}
+            deleteItem={this.deleteItem}
+            setUpdate={this.setUpdate}
+          />
         </header>
       </div>
     );
